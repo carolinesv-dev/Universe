95d3e75507ea658e14698456a994e9810b4f24cd
Carol Silveira <carolinesv.dev@gmail.com>
2024-08-03 17:56:12 -0300

    first

--- js/actions.js
+++ js/actions.js
@@ -17,19 +17,18 @@ window.addEventListener('load', updatePageClass) // qdo page carregar
 window.addEventListener('popstate', updatePageClass) // qdo nav mudar
 
 
-export function clickNav () {
-  document.addEventListener('DOMContentLoaded', function () {
-    // Obtém a URL da página atual
-    const currentUrl = window.location.pathname;
-  
-    // Seleciona todos os links de navegação
-    const navLinks = document.querySelectorAll('#navigation a');
-  
-    navLinks.forEach(function(link) {
-        // Verifica se o href do link corresponde à URL da página atual
-        if (link.getAttribute('href') === currentUrl) {
-            link.classList.add('active'); // Adiciona a classe 'active' ao link
-        }
-    });
+export function clickNav() {
+  const currentUrl = window.location.href;
+  const navLinks = document.querySelectorAll('#navigation a');
+
+  navLinks.forEach(function(link) {
+    if (link.href === currentUrl) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active'); // Opcional: remover a classe 'active' dos links não correspondentes
+    }
   });
 }
+
+// Garante que clickNav seja chamado quando o DOM estiver completamente carregado
+document.addEventListener('DOMContentLoaded', clickNav);
